4144eed56cbd1116bdd07ac47898f8ddf5f27ed1
Diogo Gomes <diogogomescastro@outlook.com>
2023-09-17 16:18:20 -0300

    Crete class pessoa

---
+++ src/trabalho1_poo_ads/Pessoa.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalho1_poo_ads;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author diogo
+ */
+public class Pessoa {
+    int id;
+    String name;
+    String sex;
+    LocalDate dateBorn;
+    String email;
+    String password;
+    //PERGUNTAR EDUARDO
+    String typeUser;
+    LocalDate dateCrete;
+    LocalDate dateModify;
+}
